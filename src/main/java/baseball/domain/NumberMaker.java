b0879bf5317e843ebf8ab89452586d75122152dd
wjh2012 <wjh2012@naver.com>
2022-11-30 13:09:13 +0900

    feat(NumberMaker): 중복되지 않는 3개의 숫자를 생성한다.

---
+++ src/main/java/baseball/domain/NumberMaker.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+import baseball.BaseBallNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberMaker {
+
+    private static final int NUMBER_SIZE = 3;
+
+    private final BaseBallNumberGenerator baseBallNumberGenerator;
+
+    public NumberMaker(BaseBallNumberGenerator baseBallNumberGenerator) {
+        this.baseBallNumberGenerator = baseBallNumberGenerator;
+    }
+
+    public List<Integer> makeNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < NUMBER_SIZE) {
+            int randomNumber = baseBallNumberGenerator.generate();
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+    private int makeOneNumber(){
+        return baseBallNumberGenerator.generate();
+    }
+}
